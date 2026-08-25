9d78f0d6ac23c3d4820f1b0d9251ca1c558e2c7d
Edvin Grasic <edvin_grasic@hotmail.com>
2026-08-25 14:05:41 +0200

    Add project files.

---
+++ OpenTimer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OpenTimer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
